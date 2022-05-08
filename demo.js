dea9785ad58ab92c2e067c6991ad28404125dbc9
zaid0813 <zaid.hussain@aventior.com>
2022-05-08 18:33:50 +0530

    initial commit

---
+++ demo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// console.log("hammad");
+
+// // const checkWordCount = function(name1, countLett){
+// //     const nameArr = name1.filter(alp => alp === countLett)
+// //     return `${nameArr }: ${countLett.length}` 
+    
+// // }
+
+// // const arr = new Set(...name1);
+// // console.log(arr);
+
+// function check(letter,word){
+//     const counts = [...word].filter(alphabet=>  alphabet === letter)
+//     console.log(`letter ${letter} - ${counts.length}`)
+// }
+
+// const uniqueAlp = function(str){
+
+//     const [...arr] = new Set(str);
+
+//     const arr2 = arr;
+
+//     for(const x of arr2){
+//         check(x, str)
+//     }
+ 
+// }
+
+// uniqueAlp("mannnnnan")
+
+
+
+
+
+
+
